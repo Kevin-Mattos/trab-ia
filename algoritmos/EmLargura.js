90ceeab2fd2d6956f6e2a3a2288ef3eae2c1dbd8
Kevin-Mattos <kevinmmattos@gmail.com>
2021-11-01 22:28:31 -0300

    impl busca em Largura

---
+++ algoritmos/EmLargura.js
@@ -0,0 +1,125 @@
+let Robo = require("../modelos/Robo")
+let CentroDistribuicao = require("../modelos/CentroDistribuicao")
+const Indice = require("../modelos/Indice");
+const tipos = require("../modelos/Tipos");
+
+function BuscaLargura(board, indiceAtual, indiceParaIr) {
+
+    let preparedBoard = new BuscaLarguraPreparedBoard(board.board)
+
+    var fila = [], resultado = [];
+    fila.push(indiceAtual);
+    while (fila.length > 0) {
+        let indice = fila.shift()
+        preparedBoard.getItem(indice).visitado = true
+        let vizinhos = preparedBoard.obterLocaisPossiveisParaIr(indice)
+        for (let i = 0; i < vizinhos.length; i++) {
+            let vizinho = vizinhos[i]
+            let item = preparedBoard.getItem(vizinho)
+            if(vizinho.coordenadaX == indiceParaIr.coordenadaX
+                && vizinho.coordenadaY == indiceParaIr.coordenadaY){
+                    console.log("chegou")
+                    var curr = item
+                    curr.parent = preparedBoard.getItem(indice);
+                    var ret = [];
+                    while (curr.parent) {
+                        ret.push(curr.indice);
+                        curr = curr.parent;
+                    }
+                    return ret.reverse();
+                }
+            if(!item.visitado) {
+                item.visitado = true
+                item.parent = preparedBoard.getItem(indice)
+                fila.push(vizinho)
+            }
+        }
+        preparedBoard.getItem(indice).visitado = true
+    }
+    return []
+}
+
+function temNaLista(lista, indice) {
+    for (var i = 0; i < lista.length; i++) {
+        var indiceLista = lista[i];
+        if (indice.coordenadaX == indiceLista.coordenadaX
+            && indice.coordenadaY == indiceLista.coordenadaY) {
+            return true
+        }
+    }
+    return false
+}
+
+class BuscaLarguraPreparedBoard {
+    constructor(board) {
+        this.board = Array(board.length).fill().map(() => Array(board[0].length).fill())
+        board.forEach((element, linha) => {
+            element.forEach((item, coluna) => {
+                if (item == undefined) {
+                    this.board[linha][coluna] = {
+                        tipo: tipos.CAMINHO,
+                        indice: new Indice(linha, coluna),
+                        visitado : false,
+                        distancia : 0,
+
+                        getTipo() {
+                            return this.tipo
+                        }
+                    }
+                } else {
+                    item.visitado = false
+                        item.distancia = 0
+                    this.board[linha][coluna] = item
+                }
+            }
+
+            )
+        });
+    }
+
+    obterLocaisPossiveisParaIr(posAtual) {
+        let pos = []
+        let possivelPos = [
+            new Indice( //lado direito
+                posAtual.coordenadaX + 1,
+                posAtual.coordenadaY
+            ),
+            new Indice( // cima
+                posAtual.coordenadaX,
+                posAtual.coordenadaY + 1
+            ),
+            new Indice( // lado Esquerdo
+                posAtual.coordenadaX - 1,
+                posAtual.coordenadaY
+            ),
+            new Indice( // para baixo
+                posAtual.coordenadaX,
+                posAtual.coordenadaY - 1
+            ),
+        ]
+
+        possivelPos.forEach((indice) => {
+            if (indice.coordenadaX < 0 || indice.coordenadaX >= 13) {
+
+            }
+            else if (indice.coordenadaY < 0 || indice.coordenadaY >= 15) {
+
+            }
+            else {
+                let item = this.board[indice.coordenadaX][indice.coordenadaY]
+                if (item.getTipo() == tipos.CAMINHO) {
+                    pos.push(indice)
+                }
+            }
+        })
+
+        return pos
+    }
+
+    getItem(indice) {
+        return this.board[indice.coordenadaX][indice.coordenadaY]
+    }
+}
+
+
+module.exports = BuscaLargura
